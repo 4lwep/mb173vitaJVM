f27a584fe0a67ec0ed935ef5bae9fd11d8a4c16a
4lwep <alfrevr05@gmail.com>
2025-07-09 17:22:23 +0200

    Añadidos todos los tags de la constant pool

--- main.c
+++ main.c
@@ -10,13 +10,13 @@ typedef struct {
     unsigned short tag;
     union {
         struct {
-            unsigned int length;
-            char *text;
-        } utf8;
-
-        struct {
             unsigned int name_index;
         } class;
+
+        struct {
+            unsigned int class_index;
+            unsigned int name_and_type_index
+        } fieldref;
 
         struct {
             unsigned int class_index;
@@ -24,11 +24,72 @@ typedef struct {
         } methodref;
 
         struct {
+            unsigned int class_index;
+            unsigned int name_and_type_index;
+        } interfaceMethodref;
+
+        struct{
+            unsigned int string_index;
+        }string;
+
+        struct{
+            unsigned int bytes;
+        }integer;
+
+        struct{
+            unsigned int bytes;
+        }_float;
+
+        struct{
+            unsigned int high_bytes;
+            unsigned int low_bytes;
+        }_long;
+
+        struct{
+            unsigned int high_bytes;
+            unsigned int low_bytes;
+        }_double;
+
+        struct {
             unsigned int name_index;
             unsigned int descriptor_index;
         } nameAndType;
 
-        // Agregar más tags
+        struct {
+            unsigned int length;
+            char *text;
+        } utf8;
+        
+        struct{
+            unsigned short reference_kind;
+            unsigned int reference_index;
+        }methodHendle;
+
+        struct{
+            unsigned int descriptor_index;
+        }methodType;
+
+        struct{
+            unsigned int bootstrap_method_attr_index;
+            unsigned int name_and_type_index;
+        }dynamic;
+
+        struct{
+            unsigned int bootstrap_method_attr_index;
+            unsigned int name_and_type_index;
+        }invokeDynamic;
+
+        struct{
+            unsigned int name_index;
+        }module;
+
+        struct{
+            unsigned int name_index;
+        }module;
+
+        struct{
+            unsigned int name_index;
+        }package;
     } info;
 } ConstantPoolEntry;
 
