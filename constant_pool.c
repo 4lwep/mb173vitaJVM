ba323467713642c74df0b9ea5e3785ceecc5d667
4lwep <alfrevr05@gmail.com>
2025-07-11 20:54:05 +0200

    Implementados todos los tags de manera básica

--- constant_pool.c
+++ constant_pool.c
@@ -19,19 +19,53 @@ void parseConstantPool(FILE *r){
     constantPool[i].tag = tag;
 
     switch(constantPool[i].tag){
-      case 7:{
+      case 7:{ //class
         constantPool[i].info.CONSTANT_class.name_index = loadU16(r);
         break;
       }
-      case 9:{
+      case 9:{ //fieldref
         constantPool[i].info.CONSTANT_fieldref.class_index = loadU16(r);
         constantPool[i].info.CONSTANT_fieldref.name_and_type_index = loadU16(r);
         break;
       }
-      case 10:{
+      case 10:{ //methodref
         constantPool[i].info.CONSTANT_methodref.class_index = loadU16(r);
         constantPool[i].info.CONSTANT_methodref.name_and_type_index = loadU16(r);
         break;
+      }
+      case 11:{ //interfaceMethodref
+        constantPool[i].info.CONSTANT_interfaceMethodref.class_index = loadU16(r);
+        constantPool[i].info.CONSTANT_interfaceMethodref.name_and_type_index = loadU16(r);
+        break;
+      }
+      case 8:{ //string
+        constantPool[i].info.CONSTANT_string.string_index = loadU16(r);
+        break;
+      }
+      case 3:{ //integer
+        constantPool[i].info.CONSTANT_integer.bytes = loadU32(r);
+        break;
+      }
+      case 4:{ //float --- Hay que hacer algunas cosas aún
+        constantPool[i].info.CONSTANT_float.bytes = loadU32(r);
+        break;
+      }
+      case 5:{ //long
+        constantPool[i].info.CONSTANT_long.high_bytes = loadU32(r);
+        constantPool[i].info.CONSTANT_long.low_bytes = loadU32(r);
+        break;
+      }
+      case 6:{ //double --- Falta especificar algunos casos como con los float
+        constantPool[i].info.CONSTANT_double.high_bytes = loadU32(r);
+        constantPool[i].info.CONSTANT_double.low_bytes = loadU32(r);
+        break;
+      }
+      case 12:{ //nameAndType
+        constantPool[i].info.CONSTANT_nameAndType.name_index = loadU16(r);
+        constantPool[i].info.CONSTANT_nameAndType.descriptor_index = loadU16(r);
+      }
+      case 1: { //utf8
+
       }
       default: break;
     }
