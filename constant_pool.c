41926e7fdf4b38cd991fd184e81df4dd21053de3
4lwep <alfrevr05@gmail.com>
2025-07-11 19:41:04 +0200

    Comentario de comando para ejecutar minecraft desde el jar

--- constant_pool.c
+++ constant_pool.c
@@ -6,17 +6,27 @@
 ConstantPoolEntry *constantPool;
 
 void parseConstantPool(FILE *r){
-    unsigned int constantPoolCount = loadU16(r);
+  unsigned int constantPoolCount = loadU16(r);
 
-    printf("Constant pool count: ");
-    printf("%d", constantPoolCount);
-    printf("\n");
+  printf("Constant pool count: ");
+  printf("%d", constantPoolCount);
+  printf("\n");
 
-    constantPool = malloc(sizeof(ConstantPoolEntry) * constantPoolCount - 1);
+  constantPool = malloc(sizeof(ConstantPoolEntry) * constantPoolCount - 1);
 
-    for (int i = 0; i < constantPoolCount; i++){
-        unsigned int tag = loadU8(r);
-        constantPool[i].tag = tag;
-    }
-    
+  for (int i = 0; i < constantPoolCount; i++){
+    unsigned int tag = loadU8(r);
+    constantPool[i].tag = tag;
+  }
 }
+
+/*
+java \    
+  -Dorg.lwjgl.librarypath=/home/alfre/Downloads/lwjgl-2.9.3/native/linux \
+  -cp "/home/alfre/Games/PrismLauncher/libraries/com/mojang/minecraft/b1.7.3/minecraft-b1.7.3-client.jar:\
+/home/alfre/Downloads/lwjgl-2.9.3/jar/lwjgl.jar:\
+/home/alfre/Downloads/lwjgl-2.9.3/jar/lwjgl_util.jar:\
+/home/alfre/Downloads/lwjgl-2.9.3/jar/jinput.jar" \
+  net.minecraft.client.Minecraft
+
+*/
