4b36569bad3bd4e73a32cd3ccae4606c403d3981
4lwep <alfrevr05@gmail.com>
2025-07-12 21:21:29 +0200

    Quitado comentario con prints de debug

--- loader/loader.c
+++ loader/loader.c
@@ -5,7 +5,6 @@
 ClassFile *parsedClass;
 
 void loadClass(FILE *r){
-    
     unsigned int signature = loadU32(r);
     unsigned int minorVersion = loadU16(r);
     unsigned int majorVersion = loadU16(r);
@@ -18,7 +17,8 @@ void loadClass(FILE *r){
 
     parseInterfaces(r);
 
-    parsedClass = malloc(sizeof(unsigned int) * 8 + sizeof(ConstantPoolEntry) * constantPoolCount);
+    parsedClass = malloc(sizeof(ClassFile));
+    parsedClass->constant_pool = malloc(sizeof(ConstantPoolEntry) * constantPoolCount);
 
     parsedClass->magic = signature;
     parsedClass->minor_version = minorVersion;
@@ -29,26 +29,4 @@ void loadClass(FILE *r){
     parsedClass->this_class = thisClass;
     parsedClass->super_class = superClass;
     parsedClass->interfaces_count = interfaceCount;
-
-
-    //Debug
-    /*printf("Signature: ");
-    printf("%08x", signature);
-    printf("\n");
-
-    printf("Version: ");
-    printf("%d.%d -> Java %d", majorVersion, minorVersion, majorVersion - 44);
-    printf("\n\n");
-
-    printf("Tag: %d, Ref index: %d\n", constantPool[0].tag, constantPool[0].info.CONSTANT_methodref.class_index - 1);
-    printf("Tag: %d, Ref index: %d\n", constantPool[1].tag, constantPool[1].info.CONSTANT_class.name_index - 1);
-    printf("Tag: %d, text: %s\n\n", constantPool[3].tag, constantPool[3].info.CONSTANT_utf8.text);
-
-    printf("Tag: %d, Ref index: %d\n", constantPool[6].tag, constantPool[6].info.CONSTANT_class.name_index - 1);
-    printf("Tag: %d, text: %s\n", constantPool[7].tag, constantPool[7].info.CONSTANT_utf8.text);
-
-    printf("Access flag: 0x%x\n", accessFlags);
-    printf("This class: %d\n", thisClass - 1);
-    printf("Super class: %d\n", superClass - 1);*/
-    //////
 }
