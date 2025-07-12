6c1ce096dcf0263f449dae58f7646d513df0bd7d
4lwep <alfrevr05@gmail.com>
2025-07-12 19:35:11 +0200

    Añadidas más propiedades del .class al parser

--- main.c
+++ main.c
@@ -16,6 +16,11 @@ int main(){
     
     parseConstantPool(r);
 
+    unsigned int accessFlags = loadU16(r);
+    unsigned int thisClass = loadU16(r);
+    unsigned int superClass = loadU16(r);
+
+    //Debug
     printf("Signature: ");
     printf("%08x", signature);
     printf("\n");
@@ -24,13 +29,16 @@ int main(){
     printf("%d.%d -> Java %d", majorVersion, minorVersion, majorVersion - 44);
     printf("\n\n");
 
-    //Debug
     printf("Tag: %d, Ref index: %d\n", constantPool[0].tag, constantPool[0].info.CONSTANT_methodref.class_index - 1);
     printf("Tag: %d, Ref index: %d\n", constantPool[1].tag, constantPool[1].info.CONSTANT_class.name_index - 1);
     printf("Tag: %d, text: %s\n\n", constantPool[3].tag, constantPool[3].info.CONSTANT_utf8.text);
 
     printf("Tag: %d, Ref index: %d\n", constantPool[6].tag, constantPool[6].info.CONSTANT_class.name_index - 1);
     printf("Tag: %d, text: %s\n", constantPool[7].tag, constantPool[7].info.CONSTANT_utf8.text);
+
+    printf("Access flag: 0x%x\n", accessFlags);
+    printf("This class: %d\n", thisClass - 1);
+    printf("Super class: %d\n", superClass - 1);
     //////
 
     fclose(r);
