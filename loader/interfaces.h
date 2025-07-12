7a551a83a2425e408c8df797cf48425767246620
4lwep <alfrevr05@gmail.com>
2025-07-12 22:31:11 +0200

    Corrección

--- loader/interfaces.h
+++ loader/interfaces.h
@@ -1,6 +1,6 @@
 #include<util.h>
 
-//Estas variable to las tengo que liberar si las libero desde la structura
+//Estas variables no las tengo que liberar si las libero desde la structura
 extern unsigned int interfaceCount;
 extern unsigned int *interface;
 
